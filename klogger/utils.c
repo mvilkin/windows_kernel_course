54c14dbc74c5f01688b511d4ac6e2b5b573049e5
mvilkin <vil_ms@mail.ru>
2017-01-22 17:59:08 +0300

    fix missing file

--- klogger/utils.c
+++ klogger/utils.c
@@ -2,61 +2,97 @@
 #include <stdlib.h>
 #include "utils.h"
 
-void* open_file(const char* name)
+HANDLE open_file(const char* name)
 {
-	/*if (!name)
+	HANDLE hfile;
+	UNICODE_STRING name_unicode;
+	OBJECT_ATTRIBUTES attribs;
+	NTSTATUS status;
+	IO_STATUS_BLOCK status_block;
+
+	if (!name)
 		return NULL;
 
-	HANDLE hfile = CreateFileA(name, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hfile == INVALID_HANDLE_VALUE)
-	{
-		printf("Error opening file: 0x%x\n", GetLastError());
+	RtlInitUnicodeString(&name_unicode, name);
+
+	InitializeObjectAttributes(&attribs, &name_unicode,
+		OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE, NULL, NULL);
+
+	status = ZwCreateFile(&hfile,
+		GENERIC_WRITE,
+		&attribs, &status_block, NULL,
+		FILE_ATTRIBUTE_NORMAL,
+		FILE_SHARE_READ,
+		FILE_OVERWRITE_IF,
+		FILE_SYNCHRONOUS_IO_NONALERT,
+		NULL, 0);
+
+	if (!NT_SUCCESS(status)) {
+		DbgPrint("KLogger: open/create file error\n");
 		return NULL;
 	}
-	return hfile;*/
+
+	DbgPrint("KLogger: open/create file comleted\n");
+	return hfile;
 }
 
-void close_file(void* handle)
+NTSTATUS close_file(void* handle)
 {
-	/*if (!handle)
-		return;
-	CloseHandle(handle);*/
+	if (!handle)
+		return STATUS_SUCCESS;
+	return ZwClose(handle);
 }
 
 size_t write_file(void* handle, void* src, size_t size)
 {
-	/*size_t wr_size = 0;
-	BOOL err = WriteFile(handle, src, size, &wr_size, NULL);
-	if (err == FALSE)
-	{
-		printf("Error writing to file: 0x%x\n", GetLastError());
+	NTSTATUS status;
+	IO_STATUS_BLOCK status_block;
+
+	if (!handle || !src || !size)
+		return 0;
+
+	status = ZwWriteFile(handle, NULL, NULL, NULL,
+		&status_block, src, size, NULL, NULL);
+
+	if (!NT_SUCCESS(status)) {
+		DbgPrint("KLogger: write to file error\n");
 		return 0;
 	}
-	return wr_size;*/
+
+	DbgPrint("KLogger: write to file completed\n");
+	return status_block.Information;
 }
 
 void* alloc_memory(size_t size)
 {
-	/*if (size <= 0)
+	if (!size)
 		return NULL;
-	return malloc(size);*/
+
+	return ExAllocatePoolWithTag(NonPagedPool, size, 'Tag1');
 }
 
 void free_memory(void* ptr)
 {
-	/*if (!ptr)
+	if (!ptr)
 		return;
-	free(ptr);*/
+
+	ExFreePoolWithTag(ptr, 'Tag1');
 }
 
 void copy_memory(void* dst, void* src, size_t size)
 {
-	/*if (size <= 0 || !dst || !src)
+	if (!dst || !src || !size)
 		return;
-	memcpy(dst, src, size);*/
+
+	RtlCopyMemory(dst, src, size);
 }
 
 long atomic_add(long* shared, long value)
 {
-	//return InterlockedExchangeAdd(shared, value);
+	return InterlockedExchangeAdd(shared, value);
+}
+
+long atomic_set(long* shared, long value)
+{
+	return InterlockedExchange(shared, value);
 }
